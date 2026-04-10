b82a43e41d064cf36a0f7db27ce145d138142a55
ALBO DAVIDE <albodavidexd@gmail.com>
2026-04-10 08:48:11 +0200

    Add PannelloPrese plugboard implementation
    Introduce PannelloPrese to model the Enigma plugboard with an internal scambi[] mapping. The constructor initializes an identity mapping; configura(String) resets then parses pairs like "AB CD EF" (case-insensitive) to set bidirectional swaps; scambia(char) returns the mapped letter. Also perform a minor whitespace/newline cleanup in Riflettore.

--- MacchinaEnigma/src/main/java/com/albo/macchinaenigma/Riflettore.java
+++ MacchinaEnigma/src/main/java/com/albo/macchinaenigma/Riflettore.java
@@ -5,7 +5,7 @@ package com.albo.macchinaenigma;
 
 public class Riflettore {
     private final char[] mappatura = "YRUHQSLDPXNGOKMIEBFZCWVJAT".toCharArray();
-    
+
     public char rifletti(char c) {
         return mappatura[c - 'A'];
     }
