1ff7d703a637d641d0c0c53ba59e5bb69670fbec
ALBO DAVIDE <albodavidexd@gmail.com>
2026-04-10 08:53:46 +0200

    Add rotor selection and historic mappings
    Introduce static mappings for three historical rotors and make mappatura instance-specific. Add a Rotore(int numero) constructor to select ROTORE_I/II/III based on the given rotor number (1, 2, or 3). Move and keep the ruota(), setPosizione(), cifraAvanti(), and cifraIndietro() logic intact; this change enables configuring different rotor wirings instead of a single hardcoded mapping.

--- MacchinaEnigma/src/main/java/com/albo/macchinaenigma/Rotore.java
+++ MacchinaEnigma/src/main/java/com/albo/macchinaenigma/Rotore.java
@@ -2,14 +2,26 @@ package com.albo.macchinaenigma;
 
 public class Rotore {
 
-    private final char[] mappatura = "EKMFLGDQVZNTOWYHXUSPAIBRCJ".toCharArray();
+    // Mappature storiche dei 3 rotori
+    private static final char[] ROTORE_I   = "EKMFLGDQVZNTOWYHXUSPAIBRCJ".toCharArray();
+    private static final char[] ROTORE_II  = "AJDKSIRUXBLHWTMCQGZNPYFVOE".toCharArray();
+    private static final char[] ROTORE_III = "BDFHJLCPRTXVZNYEIWGAKMUSQO".toCharArray();
+
+    private final char[] mappatura;
 
     private int posizione = 0;  //0 = A..
 
-    public void ruota() {
-        posizione = (posizione + 1) % 26; //routa il rotore di una tacca
+    public Rotore(int numero) { //1, 2 o 3
+        switch (numero) {
+            case 2:  mappatura = ROTORE_II;  break;
+            case 3:  mappatura = ROTORE_III; break;
+            default: mappatura = ROTORE_I;   break;
+        }
     }
 
+    public void ruota() {
+        posizione = (posizione + 1) % 26; //ruota il rotore di una tacca
+    }
 
     public void setPosizione(char c) { //es: 'A' = 0
         posizione = c - 'A';
@@ -19,13 +31,11 @@ public class Rotore {
         return posizione;
     }
 
-
     public char cifraAvanti(char c) { //pos. attuale, va verso dx
         int indice = (c - 'A' + posizione) % 26;
         char cifrata = mappatura[indice];
         return (char) ((cifrata - 'A' - posizione + 26) % 26 + 'A');
     }
-
 
     public char cifraIndietro(char c) { //dopo il riflettore va verso sx
         int indice = (c - 'A' + posizione) % 26;
