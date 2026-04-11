f2f577e132f707f15bedb95c971016352589d1e9
ALBO DAVIDE <albodavidexd@gmail.com>
2026-04-11 09:30:22 +0200

    Update MacchinaEnigmaApplication.java

--- MacchinaEnigma/src/main/java/com/albo/macchinaenigma/MacchinaEnigmaApplication.java
+++ MacchinaEnigma/src/main/java/com/albo/macchinaenigma/MacchinaEnigmaApplication.java
@@ -11,7 +11,7 @@ public class MacchinaEnigmaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MacchinaEnigmaApplication.class.getResource("MacchinaEnigma-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
